8c079ee9b8f076828d6766ba4426235867f2f18f
AlbieriGarcia <albieri404@gmail.com>
2023-11-22 11:56:58 -0400

    test

--- context/database.js
+++ context/database.js
@@ -1,9 +1,9 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("railway", "root", "aADh2agCcf6dGBFGDa36hD2gDdH1a232", {
+const sequelize = new Sequelize("bkjwfva7vf46bqlev2id", "utb88pt4kioqe7so", "Q10pfuBdBuwvYXsab65t", {
   dialect: "mysql",
-  host: "monorail.proxy.rlwy.net",
-  port: 46961,
+  host: "bkjwfva7vf46bqlev2id-mysql.services.clever-cloud.com",
+  port: 3306,
 });
 
 module.exports = sequelize;
