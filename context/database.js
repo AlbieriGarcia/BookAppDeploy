8416cdd362f3cedda07e196bfa383f778c0fec63
AlbieriGarcia <albieri404@gmail.com>
2023-11-21 20:46:29 -0400

    cambio de base de datos

--- context/database.js
+++ context/database.js
@@ -1,9 +1,9 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("Bookapp", "root", "1234", {
+const sequelize = new Sequelize("railway", "root", "aADh2agCcf6dGBFGDa36hD2gDdH1a232", {
   dialect: "mysql",
-  host: "localhost",
-  port: 3306,
+  host: "monorail.proxy.rlwy.net",
+  port: 46961,
 });
 
 module.exports = sequelize;
